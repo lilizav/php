85b33bfebc0838a39f333c178d6183bcb7998de4
lilizav <vuthiliza@gmail.com>
2025-12-13 14:51:57 +0100

    lesson 11

---
+++ day 11/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head></head>
+<body>
+
+    
+    <form action="add.php" method="POST">
+
+        <input type="text" name="name" placeholder="Name">
+        <br>
+        <input type="text" name="surname" placeholder="Surname">
+        <br>
+        <input type="email" name="email" placeholder="Email">
+        <br>
+        <button type="submit" name="submit">Add</button>
+
+        <a href="dashboard.php">Go to dashboard</a>
+
+    </form>
+
+
+</body>
+</html>
