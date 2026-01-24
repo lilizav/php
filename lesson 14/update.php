e5f7373d565d9119962cf2b7048363763e50bdc8
lilizav <vuthiliza@gmail.com>
2026-01-24 05:06:34 -0800

    lesson14

--- lesson 14/update.php
+++ lesson 14/update.php
@@ -11,7 +11,7 @@ if(isset($_POST['update']))
     $email= $_POST['email'];
 
 
-    $sql = "UPDATE user set name=:name, surname=:surname, email=:email
+    $sql = "UPDATE users set name=:name, surname=:surname, email=:email
     where id=:id";
 
     $prep= $conn->prepare($sql);
